27a16539ceae76a571f24df1dcfec3825ab40778
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-13 19:08:28 +0330

    add hub

---
+++ Hubs/SiteChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_WebApplication.Hubs
+{
+    public class SiteChatHub : Hub
+    {
+        public async Task SendNewMesaage(string Sender, string Message)
+        {
+            await Clients.All.SendAsync("GetNewMessage", Sender, Message, DateTime.Now.ToShortDateString());
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
