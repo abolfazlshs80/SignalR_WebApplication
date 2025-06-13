67380960e6128a2a41e9cf07a062ff116c620cc6
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-13 20:27:29 +0330

    add service

---
+++ Service/ChatRoomService.cs
@@ -0,0 +1,36 @@
+﻿using SignalR_WebApplication.Data.Context;
+using SignalR_WebApplication.Data.Models;
+
+namespace SignalR_WebApplication.Service.SignalR.Bugeto.Models.Services;
+
+public class ChatRoomService : IChatRoomService
+{
+    private readonly AppDbContext _context;
+    public ChatRoomService(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Guid> CreateChatRoom(string ConnectionId)
+    {
+        var existChatRoom = _context.ChatRooms.SingleOrDefault(p => p.ConnectionId == ConnectionId);
+        if (existChatRoom != null)
+        {
+            return await Task.FromResult(existChatRoom.Id);
+        }
+
+        ChatRoom chatRoom = new ChatRoom()
+        {
+            ConnectionId = ConnectionId,
+            Id = Guid.NewGuid(),
+        };
+        _context.ChatRooms.Add(chatRoom);
+        _context.SaveChanges();
+        return await Task.FromResult(chatRoom.Id);
+    }
+
+    public async Task<Guid> GetChatRoomForConnection(string CoonectionId)
+    {
+        var chatRoom = _context.ChatRooms.SingleOrDefault(p => p.ConnectionId == CoonectionId);
+        return await Task.FromResult(chatRoom.Id);
+    }
+}
---
+++ Service/IChatRoomService.cs
@@ -0,0 +1,16 @@
+﻿namespace SignalR_WebApplication.Service
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    namespace SignalR.Bugeto.Models.Services
+    {
+        public interface IChatRoomService
+        {
+            Task<Guid> CreateChatRoom(string ConnectionId);
+            Task<Guid> GetChatRoomForConnection(string CoonectionId);
+        }
+    }
+
+}
