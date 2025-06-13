499c0e66867f5c8b70d1fe47d08b6e20fe1efa64
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-13 20:27:40 +0330

    add context file

---
+++ Data/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR_WebApplication.Data.Models;
+
+namespace SignalR_WebApplication.Data.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<ChatRoom> ChatRooms { get; set; }
+    }
+}
