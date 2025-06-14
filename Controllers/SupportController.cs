0cb66753c7821dc6fbe4632a8c5dbb06e30e3e2f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 09:32:03 +0330

    add suport controller

---
+++ Controllers/SupportController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_WebApplication.Controllers
+{
+    [Authorize]
+    public class SupportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
