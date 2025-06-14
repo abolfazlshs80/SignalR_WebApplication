7a2c5ef08c3ed8dc9550e7f4960b4c450d17f452
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 09:38:16 +0330

    add SupportSignalRService for chat room connection and management

---
+++ wwwroot/js/SupportSignalRService.js
@@ -0,0 +1,53 @@
+﻿
+//اتصال با هاب کانکشن پشتیبانی
+var supportConnection = new signalR.HubConnectionBuilder()
+    .withUrl('/supporthub')
+    .build();
+
+
+
+function Init() {
+    supportConnection.start();
+
+};
+
+// بعد از این که صفحه کامل بارگذاری شد  این بخش اجرا می شود
+$(document).ready(function () {
+    console.log("ready!");
+    //متد راه اندازی اجرا می شود
+    Init();
+});
+
+
+//دریافت لیست  چت روم ها
+supportConnection.on('GetRooms', loadRooms);
+
+function loadRooms(rooms) {
+    if (!rooms) return;
+    var roomIds = Object.keys(rooms);
+    if (!roomIds.length) return;
+
+
+    removeAllChildren(roomListEl);
+
+    roomIds.forEach(function (id) {
+        var roomInfo = rooms[id];
+        if (!roomInfo) return;
+
+        //ایجاد دکمه برای لیست
+        return $("#roomList").append("<a class='list-group-item list-group-item-action d-flex justify-content-between align-items-center' data-id='" + id + "' href='#'>" + roomInfo + "</a>");
+
+    });
+
+}
+
+var roomListEl = document.getElementById('roomList');
+
+
+function removeAllChildren(node) {
+    if (!node) return;
+
+    while (node.lastChild) {
+        node.removeChild(node.lastChild);
+    }
+}
