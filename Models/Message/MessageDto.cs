56c54a353cc88300add1c03db07048d430d7aa93
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 09:37:38 +0330

    add MessageDto model for message handling

---
+++ Models/Message/MessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalR_WebApplication.Models.Message;
+
+public class MessageDto
+{
+    public string Sender { get; set; }
+    public string Message { get; set; }
+    public DateTime Time { get; set; }
+}
