f774357975b6e1e86eb1c048961667182183a050
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 09:37:47 +0330

    add IAuthenticationService and AuthenticationService implementation for user login

---
+++ Service/IAuthenticationService.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using SignalR_WebApplication.Data.Models;
+
+namespace SignalR_WebApplication.Service
+{
+    public interface IAuthenticationService
+    {
+        Task<(ClaimsPrincipal, AuthenticationProperties, string)> LoginAsync(User user);
+    }
+
+    public class AuthenticationService(IUserService _userService) : IAuthenticationService
+    {
+        public async Task<(ClaimsPrincipal, AuthenticationProperties,string)> LoginAsync(User user)
+        {
+          
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.NameIdentifier, user.id.ToString())
+                };
+
+                var identity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+                var properties = new AuthenticationProperties
+                {
+                    RedirectUri = "/support"
+                };
+                return (new ClaimsPrincipal(identity),
+                        properties,
+                        CookieAuthenticationDefaults.AuthenticationScheme);
+
+            
+
+           
+        }
+    }
+
+}
