197f365aedac1178fbcf557b0ea73fa701d144b2
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 09:32:50 +0330

    add modelUser

---
+++ Data/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR_WebApplication.Data.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
