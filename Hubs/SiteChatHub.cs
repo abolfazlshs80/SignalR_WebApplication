0c2e33754272db6e61c74e186e7a64373ecbaaeb
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 12:41:00 +0330

    add authentication checks and room management methods in SiteChatHub

--- Hubs/SiteChatHub.cs
+++ Hubs/SiteChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using SignalR.Bugeto.Models.Services;
 using SignalR_WebApplication.Models.Message;
 using SignalR_WebApplication.Service.SignalR.Bugeto.Models.Services;
@@ -26,13 +27,40 @@ namespace SignalR_WebApplication.Hubs
 
         public override async Task OnConnectedAsync()
         {
-
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                await base.OnConnectedAsync();
+                return;
+            }
             var roomId = await _chatRoomService.CreateChatRoom(Context.ConnectionId);
 
             await Groups.AddToGroupAsync(Context.ConnectionId, roomId.ToString());
             await Clients.Caller.
                 SendAsync("getNewMessage", "پشتیبانی ", "سلام وقت بخیر 👋 . چطور میتونم کمکتون کنم؟", DateTime.Now.ToShortTimeString());
             await base.OnConnectedAsync();
+        }
+
+        /// <summary>
+        /// پیوستن پشتیبان ها به گروه
+        /// </summary>
+        /// <param name="roomId"></param>
+        /// <returns></returns>
+        /// 
+        [Authorize]
+        public async Task JoinRoom(Guid roomId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId.ToString());
+        }
+
+        /// <summary>
+        /// ترک گروه توسط پشتیبان
+        /// </summary>
+        /// <param name="roomId"></param>
+        /// <returns></returns>
+        [Authorize]
+        public async Task LeaveRoom(Guid roomId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId.ToString());
         }
 
         public override Task OnDisconnectedAsync(Exception? exception)
