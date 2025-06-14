33af5fcbe772f128cb100c06d08bb31f578f58f4
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 09:33:37 +0330

    add suporthub

---
+++ Hubs/SupportHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using SignalR_WebApplication.Service.SignalR.Bugeto.Models.Services;
+
+namespace SignalR_WebApplication.Hubs
+{
+    [Authorize]
+    public class SupportHub:Hub
+    {
+        private readonly IChatRoomService _chatRoomService;
+
+        public SupportHub(IChatRoomService chatRoomService)
+        {
+            _chatRoomService = chatRoomService;
+        }
+        public async override Task OnConnectedAsync()
+        {
+            var rooms = await _chatRoomService.GetAllrooms();
+            await Clients.Caller.SendAsync("GetRooms", rooms);
+            await base.OnConnectedAsync(); 
+        }
+    }
+}
