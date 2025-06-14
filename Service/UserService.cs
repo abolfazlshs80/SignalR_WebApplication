2e7b5a17210126f28352d6fa227596de80c88fe1
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 09:37:57 +0330

    add IUserService interface and UserService implementation for user validation

---
+++ Service/UserService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR_WebApplication.Data.Context;
+using SignalR_WebApplication.Data.Models;
+
+namespace SignalR_WebApplication.Service
+{
+
+    public interface IUserService
+    {
+        Task<User> ValidateUser((string userName, string password) model);
+    }
+    public class UserService(AppDbContext _context): IUserService
+    {
+        public async Task<User> ValidateUser((string userName, string password) model)
+        {
+            return await _context.User.SingleOrDefaultAsync(p => p.UserName == model.userName && p.Password == model.password);
+
+        }
+    }
+}
