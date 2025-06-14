d24501f178a94d6be0f84d0ad8c4196df1d5f2c6
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-14 09:33:26 +0330

    refactor sitechatHub and use IMessageService

--- Hubs/SiteChatHub.cs
+++ Hubs/SiteChatHub.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.Bugeto.Models.Services;
+using SignalR_WebApplication.Models.Message;
 using SignalR_WebApplication.Service.SignalR.Bugeto.Models.Services;
 
 namespace SignalR_WebApplication.Hubs
 {
-    public class SiteChatHub(IChatRoomService _chatRoomService) : Hub
+    public class SiteChatHub(IChatRoomService _chatRoomService,IMessageService _messageService) : Hub
     {
         public async Task SendNewMessage(string Sender, string Message)
         {
             var roomId = await _chatRoomService.GetChatRoomForConnection(Context.ConnectionId);
+            MessageDto messageDto = new MessageDto()
+            {
+                Message = Message,
+                Sender = Sender,
+                Time = DateTime.Now,
+            };
 
+            await _messageService.SaveChatMessage(roomId, messageDto);
             await Clients.Groups(roomId.ToString())
-                .SendAsync("GetNewMessage", Sender, Message, DateTime.Now.ToShortDateString());
+                .SendAsync("GetNewMessage", messageDto.Sender, messageDto.Message, messageDto.Time.ToShortDateString());
+          
           
         }
 
@@ -29,5 +39,11 @@ namespace SignalR_WebApplication.Hubs
         {
             return base.OnDisconnectedAsync(exception);
         }
+
+
+
+  
+
+
     }
 }
